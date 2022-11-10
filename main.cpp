58bd97ecd06e1e09674b8ebd8ff4851a1f438fe5
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-11-10 18:56:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
+float a=0, b=0, c=0;
 
-cin >> a >> b >> c;
+   cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
+  cout << ((a*b)/2) <<endl; //area del triangolo a=base e b=altezza
+  cout << (a*a) <<endl; // area del quadrato a=lato 
+  cout << a*b <<endl; // area del rettangolo a=base e b=altezza
+  cout << ((a+b)*c)/2 <<endl; // area del trapezio a=base minore, b=base maggiore e c=altezza
 
 
 }
